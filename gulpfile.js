c0c8e1cea2e47a9b9928d900390862b2b42ca48b
Mario Finelli <mtfinelli@gmail.com>
2015-12-28 21:42:12 -0500

    Temporarily switch to gulp-minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 var html = require('gulp-htmlmin');
 var jade = require('gulp-jade');
-var nano = require('gulp-cssnano');
+var nano = require('gulp-minify-css');
 var sass = require('gulp-sass');
 
 var concat = require('gulp-concat');
